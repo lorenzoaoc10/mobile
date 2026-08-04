6a1e5f401723a29f2799f8f16460d3c20ed171cb
lorenzoaoc10 <lorenzo.fierro@escola.pr.gov.br>
2026-08-04 18:12:08 -0300

    aula 6

---
+++ aula 6/ex1.js
@@ -0,0 +1,9 @@
+const celsius = [0, 25, 37, 100, -10];
+
+const fahrenheit = celsius.map((temperatura)=> {
+    return (temperatura * 1.8 + 32)
+})
+
+console.log(celsius);
+console.log(fahrenheit)
+console.log(0.1 + 0.3)
